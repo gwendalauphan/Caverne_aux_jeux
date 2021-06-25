2223355b470f66dee794ecd4bb55c76b7c9b6080
Gwendal Auphan <38136863+gwendal95@users.noreply.github.com>
2021-06-25 17:51:35 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# Plateforme_de_jeu_Python
-Plateforme de jeu
+# ISN_Projet_Final
+Répertoire avec le projet d'ISN de fin d'année
+
+plein de mini-jeux
+-> morpion, snake, puissance 4, pac man, tetris, robot tete chercheuse, qui-est-ce?, space invaders,
+pendu, pong, demineur, phantôme
+
+## mardi 26 mars 2019:  
+début du projet et assemblage des idées
+
+début de la tete chercheuse
+
+Mise en place du repository GitHub avec syncronisation entre les ordinateurs.
+
+## mercredi 27 mars 2019:
+début réseau: Script pour transmetre un dictionnaire par TCP connection possible par pusieurs clients et la connection/déconnection est gérée
+
+Finalisation du jeu de tête chercheuse
+
+début de la création de l'interface principale
+
+## Jeudi 28 mars 2019:
+Bug de duplication du robot après 2ème essai
+Bug : Inversement du sens du robot après 2ème essai
+Bug sur le timer
+
+Ajout de textures pour le jeux tête chercheuse et ajout pièces pour avoir un bonus de point
+
+### hébergement du serveur sur raspberry: le serveur est acessible en permanence
+
+
+## Vendredi 29 mars 2019:
+ajout de l'interface avec les règles du jeux et gestion de la fin ajout de la page du scoreboard au début du jeu
+
+envoi du scoreboard avec les 10 meilleurs joueurs et stockage des données dans un fichier
+
+Amélioration de la page des rêgles
+
+## Samedi 30 mars 2019:
+création de la classe scoreboard
+
+création d'une fonction pour avoir les 10 meilleurs joueurs dans un jeu spécifique
+
+## Dimanche 31 mars 2019:
+création du classement Total dans la page principale
+
+Ajout de la demande du pseudo au départ
+
+serveur: ajout d'une fonction pour récupérer le score d'un joueur en particulier
+
+## Lundi 1 avril 2019:
+Finalisation des détails de tete chercheuse, il ne rest plus qu'à créer une jolie interface graphique
+Début de Snake: création du jeu a l'aide d'une classe englobant tout le jeux
+Recherche et découpage des images du serpent ainsi que de la pomme.
+
+## Mardi 2 avril 2019:
+Snake: affichage des images fin de la mécanique de jeux
+
+### Tete chercheuse fini
+Snake fonctionnel avec gestion du score et mort
+Début de Fantome, mise en place de la grille et du niveau (Bug le liste)??
+
+## Mercredi 3 avril 2019:
+Continuer Fantome et finir Snake
+Gros Bug d'algo de fantome, commencement du démineur.
+Faire l'inversement de Tom(image)
+
+## Jeudi 4 avril 2019:
+Penser à la bombe du snake en dessous de 50 de longueur/Faire peut etre une fin au cas où on gagnerait la partie/ penser a pouvoir changer les touches
+
+Graphique du snake en devellopement
+
+Fin des règles du snake
+
+fin du système de jeux du démineur
+
+## Vendredi 5 avril 2019:
+débug serveur, début de Pendu, interface de Snake et Fantome, ajout de la liste de mots français
+
+## Dimanche 7 avril 2019:
+Fin du pendu, le jeu est totalement fonctionnel
+
+### Snake finis
+
+## Lundi 8 avril 2019:
+début et fin des mécaniques de snake
+
+explication du fonctionnement de pong et création des fichiers
+
+## Mercredi 18 Avril
+reprise du projet
+ajout des meilleurs joueurs pour le snake et le tetris
+
+partie graphique améliorée
+
+## Dimanche 21 Avril 2019
+
+amélioration de l'algorithme de placement des bombes démineur
+
+### Fantome finis
+
+## Lundi 22 Avril 2019:
+
+correction algorithme démineur, correction niveaux Tetris
+
+ajout de la barre de scroll dans le menus principal pour faire défiler les jeux
+
+## Jeudi 25 Avril 2019:
+
+Début de Flappy Bird
+
+## Luni 28 Avril 2019:
+
+Fin du jeu Flappy Bird avec commentaires détaillés
+
+Début de la partie aide dans le menu
+
+
+
+C'est moi qui est tout pensé sur comment on allait agencer les jeux, le score board, les regles, la disposition des element, le titre, les couleurs
+
+et je suis 100% daccord que t as fait le reseau
---
+++ main.py
@@ -0,0 +1,181 @@
+from tkinter import *
+from Reseau.client import *
+from tkinter import font
+sys.path.append('../')
+from Reseau.client import *
+
+from Tete_chercheuse.tete_chercheuse import *
+from Snake.Snake_main import *
+from Fantome.Fantome_main import *
+from Minesweeper.minesweeper import *
+from Pendu.pendu import *
+from Tetris.tetris import *
+from Pong.Pong_main import*
+from Flappy_Bird.Flappy_Bird_main import*
+
+class BoutonS: #classe pour gérer les boutons interactifs
+    def __init__(self, x, y, jeux, run, name): # a besoin de ligne, colone, ne nom du jeux et la commande our executer le jeu
+        self.image = PhotoImage(file = "thumbnail/" + jeux + ".png") #on charge l'immage correspondante au jeu
+        Frame_main.create_text(y*(450/5) + 20, x*(265/4)- 90, text = name, font = ("Berlin Sans FB", 20), fill = "white")
+        self.created = Frame_main.create_image(y*(450/5) + 20, x*(265/4), image = self.image)
+        Frame_main.tag_bind(self.created, "<Button-1>", self.command)
+        self.jeux = jeux
+        self.run = run
+
+    def command(self, event = None): #fonction executée lors du clique sur le boutton
+        global label_pseudo, label_score, score
+        root_main.withdraw() #on masque l'interface principale
+        result = self.run(User_name) #on execute le jeu
+        push_score(User_name, self.jeux, result) #on envois au serveur le score de la partie
+        root_main.deiconify() #on fait réapparaite la fenetre principale
+        Frame_main['yscrollcommand'] = scrollY.set #rappel de la bare de scroll
+
+        for label in label_score:
+            label.destroy()
+        for label in label_pseudo:
+            label.destroy()
+
+        score = get_score_list()
+
+        label_pseudo = []
+        label_score = []
+        for i in range(len(score)): #pour chaque éléments de la liste recue, on affiche le pseudo et le score
+            label_pseudo.append(Label(Frame_ranking,text = "#{} :       {}".format(i+1, score[i][0]),bg = '#111111', foreground = 'green2',font = ("Helvetica", 8)))
+            label_pseudo[-1].place(x = 4, y = 75 +i*20)
+            label_score.append(Label(Frame_ranking, text = "{}".format(str(int(score[i][1]))),bg = '#111111', foreground = 'green2',font = ("Helvetica", 8)))
+            label_score[-1].place(x = 160, y = 75 +i*20)
+
+        for i in range(10-len(score)): #si jamais la liste est plus petite que 10, on affiche des emplacements vides
+            label_pseudo.append(Label(Frame_ranking,text = "#{} :".format(i+1+len(score)),bg = '#111111', foreground = 'green2',font = ("Helvetica", 8)))
+            label_pseudo[-1].place(x = 4, y = 75 + 20*len(score) +i*20)
+
+User_name = "Unknown"
+
+with open('Data/mots.txt', 'r') as file:
+    mots = file.read()
+    mots_interdits = mots.split("\n")
+
+def valider(event = None):
+    global User_name
+    temp = entry.get().replace(" ", "_")
+    if temp == "" or len(temp) > 20:
+        alert.place(x = 50, y = 100)
+        return
+    for elt in mots_interdits[:-1]:
+        if elt in temp.lower():
+            alert.place(x = 50, y = 100)
+            return
+    User_name = temp
+    root_user.destroy()
+    root_user.quit()
+
+def para():
+    playground = Frame(root_main, width = 800, height = 500, bg = "#111111")
+    playground.place(x = 200, y = 100)
+    Button_para.config( image = door, command = lambda: leave_para(playground))
+
+    text = "Le but de cette application est de s'ammuser en jouant a des jeux.\nTu peux défier tes amis en comparant leur score au tien\nsur différents jeux et essayer de faire le meilleur score possible."
+    Label(playground, text = "Aide", font = ("Helvetica", 25), bg = "#111111", fg = "#888888").place(x = 300, y = 20)
+    Label(playground, text = text, font = ("Helvetica", 10), bg = "#111111", fg = "#888888").place(x = 100, y = 60)
+
+
+def leave_para(playground):
+    playground.destroy()
+    Button_para.config(image = gearImg, command = para)
+
+root_user = Tk()
+root_user.geometry("300x120")
+root_user.bind("<Return>", valider)
+root_user.focus_force()
+
+Label(root_user, text = "Entre un pseudo pour jouer").place(x = 100, y = 30)
+
+entry = Entry(root_user)
+entry.place(x = 25, y = 80)
+entry.focus()
+
+alert = Label(root_user, text = "Nom invalide", fg = "red")
+
+confirm = Button(root_user, text = "valider",cursor ='hand2', command = valider)
+confirm.place(x = 225, y = 80)
+
+root_user.mainloop()
+
+root_main = Tk()
+root_main.geometry('1020x600')
+root_main.title("Menu")
+root_main.focus_force()
+#########################-----Création de la forme de la page----------------------#######################################
+Frame_top = Frame(root_main, bg ='#111111') #création des pannels
+Frame_top.pack(ipadx = 1000, ipady =50, side = TOP)
+
+Frame_left = Frame(root_main, bg ='#111111')
+Frame_left.pack(ipadx = 100, ipady =500,side = LEFT)
+
+Frame_right = Frame(root_main, bg ='#111111')
+Frame_right.pack(ipadx = 20, ipady = 600,side = RIGHT)
+
+Frame_down = Frame(root_main, bg ='#111111')
+Frame_down.pack(ipadx = 900, ipady = 20,side = BOTTOM)
+
+image_de_fond = PhotoImage(file = "thumbnail/image_de_fond.png")
+
+Frame_main = Canvas(root_main,highlightthickness=0, borderwidth = 2, bg = '#111111', relief=GROOVE, scrollregion = (0, 0, 250, 1000))
+Frame_main.pack(ipadx = 900, ipady =530,side = BOTTOM)
+Frame_main.create_image(380, 537, image = image_de_fond)
+#Frame_main.create_image(450,889, image = image_de_fond)
+
+scrollY = Scrollbar(root_main, orient = "vertical", cursor = 'hand2', command = Frame_main.yview)
+scrollY.place(x = 962, y = 102, height = 456)
+
+Frame_main['yscrollcommand'] = scrollY.set
+Frame_main.bind("<MouseWheel>", lambda event: Frame_main.yview_scroll(int(-1*(event.delta/120)), "units"))
+
+Frame_ranking = Frame(Frame_left, width = 196 , height =280 , bg = '#111111', relief = GROOVE)
+Frame_ranking.place(x = 2, y = 70)
+
+gearImg = PhotoImage(file = 'Parametters/gear.png')
+door = PhotoImage(file = "Parametters/door.png")
+Button_para = Button(Frame_top, image = gearImg, bg = "#111111", borderwidth = 0, highlightthickness = 0, cursor = "hand2", command = para)
+Button_para.place(x = 900, y = 30)
+
+score = get_score_list() #récupération du scoreboard
+
+#############---------Création des labels et autres au contour du Frame_main-------#########################
+
+Title_main = Label(Frame_top, text = 'La Caverne Aux Jeux',font = ("Berlin Sans FB", 45), bg ='#111111', foreground = '#00e600', relief = GROOVE)
+Title_main.place(x = 300, y = 10)
+
+Title_ranking = Label(Frame_ranking, text = 'Classements',font = ("Berlin Sans FB", 20), bg = '#111111', foreground = '#00e600', relief = GROOVE)
+Title_ranking.place(x = 27, y = 5)
+
+################---------Création du Classement-----------------------------################################
+Label(Frame_ranking, text = "Rang" + " "*8 + "Nom" + " "*24 + "Score  " ,font = ("Helvetica",9), bg = '#111111', foreground = '#00e600', relief = GROOVE).place(x = 2, y = 50) #légende
+label_pseudo = []
+label_score = []
+for i in range(len(score)): #pour chaque éléments de la liste recue, on affiche le pseudo et le score
+    label_pseudo.append(Label(Frame_ranking,text = "#{} :       {}".format(i+1, score[i][0]),bg = '#111111', foreground = '#00e600',font = ("Helvetica", 8)))
+    label_pseudo[-1].place(x = 4, y = 75 +i*20)
+    label_score.append(Label(Frame_ranking, text = "{}".format(str(int(score[i][1]))),bg = '#111111', foreground = '#00e600',font = ("Helvetica", 8)))
+    label_score[-1].place(x = 160, y = 75 +i*20)
+
+for i in range(10-len(score)): #si jamais la liste est plus petite que 10, on affiche des emplacements vides
+    label_pseudo.append(Label(Frame_ranking,text = "#{} :".format(i+1+len(score)),bg = '#111111', foreground = '#00e600',font = ("Helvetica", 8)))
+    label_pseudo[-1].place(x = 4, y = 75 + 20*len(score) +i*20)
+
+for i in range(9):
+    Frame_main.rowconfigure(i, minsize = 30, pad = 20)
+    Frame_main.columnconfigure(i, minsize = 25, pad = 10)
+
+#############----------Création du tableau et des Labels du Frame_main--------------#################################
+
+bouton_0 = BoutonS(2, 1, "Tete", Tete, "Tête chercheuse")
+bouton_1 = BoutonS(2, 3, "Snake", Snake, "Snake")
+bouton_2 = BoutonS(5, 3, "Ghost", Ghost, "Tom & Jerry")
+bouton_3 = BoutonS(5, 5, "Minesweeper", Minesweeper, "Démineur")
+bouton_4 = BoutonS(2, 5, "Pendu", Pendu, "Pendu")
+bouton_5 = BoutonS(5, 1, "Tetris", Tetris, "Tetris")
+bouton_6 = BoutonS(5, 7, "Pong", Pong, "Pong")
+bouton_7 = BoutonS(2, 7, "Flappy", Flappy_Bird, "Flappy")
+
+root_main.mainloop()
